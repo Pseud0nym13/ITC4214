53bc26deb0b702f570901c1378f5731aa2d29dff
Nick Paradeisanos <n.paradeisanos@acg.edu>
2022-04-04 21:00:39 +0300

    New commit

--- cyber/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-class GamesAdmin(admin.ModelAdmin):
-    list_display=('name',)
-
-
-admin.site.register(models.Game, GamesAdmin)
-# Register your models here.
--- cyber/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-class Game(models.Model):
-    name = models.CharField(max_length=100)
-    developer = models.CharField(max_length=100)
-    genre = models.CharField(max_length=30)
-    description = models.TextField()
-    release_date = models.DateTimeField()
-# Create your models here.
