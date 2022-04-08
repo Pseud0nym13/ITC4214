5f75bb976a37e6c4e4d2532a1891b28526d49182
Nick Paradeisanos <n.paradeisanos@acg.edu>
2022-04-08 15:40:19 +0300

    new commit

--- CyberSec/urls.py
+++ CyberSec/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from cyber import views
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('login', views.LoginInterfaceView.as_view(),name = 'login'),
--- cyber/views.py
+++ cyber/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = 'register.html'
     success_url = 'home.html'
+    
 class LogoutInterfaceView(LogoutView):
     template_name ='logout.html'
 
